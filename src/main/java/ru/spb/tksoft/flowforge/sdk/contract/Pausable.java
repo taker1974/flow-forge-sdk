df5ddb95ebc5bf8e88f5e3920c52b43adf47809f
taker1974 <kostus.online.1974@yandex.ru>
2026-01-16 11:39:11 +0300

    pausable contract added

---
+++ src/main/java/ru/spb/tksoft/flowforge/sdk/contract/Pausable.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2026 Konstantin Terskikh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package ru.spb.tksoft.flowforge.sdk.contract;
+
+/**
+ * Pausable interface.
+ * 
+ * @author Konstantin Terskikh, kostus.online.1974@yandex.ru, 2026
+ */
+public interface Pausable {
+
+    /**
+     * Check if the subject is paused.
+     * 
+     * @return true if the subject is paused, false otherwise.
+     */
+    boolean isPaused();
+
+    /**
+     * Pause the subject.
+     * 
+     * This method is used to pause the subject.
+     */
+    void pause();
+
+    /**
+     * Resume the subject.
+     */
+    void resume();
+}
